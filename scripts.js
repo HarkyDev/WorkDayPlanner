fd3d19620e697f049670be02b5f588d255aedde9
Eoin Harkin <eharky@gmail.com>
2021-11-15 16:48:43 +0000

    input area changes colour based on local time

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
-const currentDay = document.getElementById('currentDay')
 const m = moment();
-console.log(m.toString())
+var currentTime = m.format("hh")
+const currentDay = document.getElementById('currentDay')
+const timerContainerValue = document.getElementById("timer-container")
+var timeValue = document.getElementById('timeValue')
+var inputArea = document.getElementById('eventText')
+var trueTimeValue = timeValue.innerHTML
+
+
+
+
+
+console.log("this is our time value " + trueTimeValue)
+console.log("this is momentJS's time Value " + currentTime)
+
+if (trueTimeValue == currentTime){
+    console.log("this is passing")
+    inputArea.classList.add('currentTime');
+}
+else {
+    console.log("Values do not match")
+}
+
+
+
+
+
 
 currentDay.textContent = m.format("dddd MMM Mo")
+
+
+
+
